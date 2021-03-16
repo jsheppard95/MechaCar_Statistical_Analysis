621ad1ac6765a047a909fba31df8241165918e1e
Jackson Sheppard <sheppard@slac.stanford.edu>
2021-03-15 22:40:14 -0700

    ENH: suspension coil summary stats for all vehicles and grouped by lot

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,8 @@
 # Import dplyr library
 library(dplyr)
 
+# MechaCar_mpg Analysis
+
 # Load "MechaCar_mpg.csv" file into dataframe
 infile <- "Resources/MechaCar_mpg.csv"
 mechacar_table <- read.csv(infile, check.names=F, stringsAsFactors=F)
@@ -14,3 +16,18 @@ model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
 model
 # Get p-value and r-squared for the linear regression model
 summary(model)
+
+# Suspension_Coil Analysis
+# Load "Suspension_Coil.csv" file into table
+infile <- "Resources/Suspension_Coil.csv"
+suspension_table <- read.csv(infile, check.names=F, stringsAsFactors=F)
+
+head(suspension_table)
+
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),
+                                                Median=median(PSI),
+                                                Variance=var(PSI),
+                                                SD=sd(PSI))
+
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))

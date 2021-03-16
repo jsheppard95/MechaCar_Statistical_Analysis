d6c859ea6a609a9107b06dace066ea26dd384437
Jackson Sheppard <sheppard@slac.stanford.edu>
2021-03-15 21:19:49 -0700

    ENH: data files and linear model for mpg = f(rest of mechacar data)

---
+++ MechaCarChallenge.R
@@ -0,0 +1,15 @@
+# Import dplyr library
+library(dplyr)
+
+# Load "MechaCar_mpg.csv" file into dataframe
+infile <- "MechaCar_mpg.csv"
+mechacar_table <- read.csv(infile, check.names=F, stringsAsFactors=F)
+
+head(mechacar_table)
+
+# Perform linear regression to predict mpg from remaining `mechacar_table`
+# data. Multiple linear regression model using `lm`
+model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
+              ground_clearance + AWD, data=mechacar_table)
+# Get p-value and r-squared for the linear regression model
+summary(model)
